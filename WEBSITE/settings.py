9c7e9a949d9b9c8601622d9855ed851bc301419a
enockzaake <enockzaake@gmail.com>
2023-02-01 17:17:10 +0530

    rm db config

--- WEBSITE/settings.py
+++ WEBSITE/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = 'WEBSITE.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ecommerce',
-        'USER':'postgres',
-        'PASSWORD':'enock',
-        'HOST':'localhost',
-        'PORT':'5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'ecommerce',
+#         'USER':'postgres',
+#         'PASSWORD':'enock',
+#         'HOST':'localhost',
+#         'PORT':'5432'
+#     }
+# }
 
 AUTH_USER_MODEL='authentication.CustomUser'
 AUTHENTICATION_BACKENDS =['authentication.backends.EmailBackend','django.contrib.auth.backends.ModelBackend',]
